6b81aec442c0dfb9f3ad70b07de6bc3498b3ad43
Michael Lippautz <michael.lippautz@gmail.com>
2013-04-16 14:25:34 +0200

    Lint again, don't use ./ prefix for includes.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/log.cc
+++ src/log.cc
@@ -2,7 +2,7 @@
 // Please see the AUTHORS file for details.  Use of this source code is governed
 // by a BSD license that can be found in the LICENSE file.
 
-#include "./log.h"
+#include "log.h"
 
 #include <stdlib.h>
 #include <stdio.h>
