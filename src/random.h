95d544374ecee294e2f5cddf8a44a7ce6c2fbed9
Michael Lippautz <michael.lippautz@gmail.com>
2013-04-19 17:35:28 +0200

    Added pseudorng based on rtdsc.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

---
+++ src/random.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2012-2013, the scalloc Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
+#ifndef SCALLOC_RANDOM_H_
+#define SCALLOC_RANDOM_H_
+
+#include <stdint.h>
+
+#include "common.h"
+
+always_inline uint64_t rdtsc(void) {
+  unsigned int hi, lo;
+  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+  return ((uint64_t) lo) | (((uint64_t) hi) << 32);
+}
+
+always_inline uint64_t hwrand() {
+  return (rdtsc() >> 6);
+}
+
+#endif  // SCALLOC_RANDOM_H_
