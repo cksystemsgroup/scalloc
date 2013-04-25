7a3e3ba2799b31b5a0b1e3705c24b4698638fec0
Michael Lippautz <michael.lippautz@gmail.com>
2013-04-25 13:53:57 +0200

    Integrate the large object allocator in scalloc.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/scalloc.cc
+++ src/scalloc.cc
@@ -1,16 +1,39 @@
+// Copyright (c) 2012-2013, the scalloc Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #include "scalloc.h"
 
+#include <errno.h>
+
+#include "allocators/large_object_allocator.h"
 #include "common.h"
 
 namespace scalloc {
 
 always_inline void* malloc(const size_t size) {
-  return NULL;
+  void* p;
+  if (size > kMaxSmallSize) {
+    // large allocation
+    p = LargeObjectAllocator::Alloc(size);
+  } else {
+    // TODO(mlippautz): small allocation
+  }
+  if (UNLIKELY(p == NULL)) {
+    errno = ENOMEM;
+  }
+  return p;
 }
 
 always_inline void free(void* p) {
   if (UNLIKELY(p == NULL)) {
     return;
+  }
+  BlockHeader* hdr = BlockHeader::GetFromObject(p);
+  if (hdr->type == kSlab) {
+    // TODO(mlippautz): small free
+  } else if (hdr->type == kLargeObject) {
+    LargeObjectAllocator::Free(reinterpret_cast<LargeObjectHeader*>(hdr));
   }
   return;
 }
--- src/scalloc.h
+++ src/scalloc.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2012-2013, the scalloc Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
 #ifndef SCALLOC_SCALLOC_H_
 #define SCALLOC_SCALLOC_H_
 
@@ -11,9 +15,9 @@
 #define ALIAS(scalloc_fn) __attribute__ ((alias(#scalloc_fn)))
 
 extern "C" {
-  void* malloc(size_t size) __THROW                               ALIAS(scalloc_malloc);
-  void free(void* p) __THROW                                      ALIAS(scalloc_free);
-  void cfree(void* p) __THROW                                     ALIAS(scalloc_free);
+  void* malloc(size_t size) __THROW                   ALIAS(scalloc_malloc);
+  void free(void* p) __THROW                          ALIAS(scalloc_free);
+  void cfree(void* p) __THROW                         ALIAS(scalloc_free);
 }
 
 #endif  // SCALLOC_SCALLOC_H_
