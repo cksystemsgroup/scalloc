c26bbbd2119278ea7abcdb33bcea3a3bfe70f346
Michael Lippautz <michael.lippautz@gmail.com>
2013-04-30 13:43:43 +0200

    Add missing aliases for a complete C allocator implementation.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/scalloc.cc
+++ src/scalloc.cc
@@ -5,6 +5,7 @@
 #include "scalloc.h"
 
 #include <errno.h>
+#include <string.h>
 
 #include "allocators/large_object_allocator.h"
 #include "allocators/dq_sc_allocator.h"
@@ -20,10 +21,13 @@ ScallocGuard::ScallocGuard() {
   if (scallocguard_refcount++ == 0) {
     RuntimeVars::InitModule();
     scalloc::SizeMap::InitModule();
-    scalloc::SlabScAllocator::InitModule();
-    scalloc::DQScAllocator::InitModule();
 
-    scalloc::PageHeap::GetHeap()->AsyncRefill();
+    scalloc::DQScAllocator::InitModule();
+    scalloc::SlabScAllocator::InitModule();
+
+    free(malloc(1));
+    scalloc::ThreadCache::InitPthread();
+    free(malloc(1));
   }
 }
 
@@ -75,4 +79,44 @@ extern "C" void* scalloc_malloc(size_t size) __THROW {
 
 extern "C" void scalloc_free(void* p) __THROW {
   scalloc::free(p);
+}
+
+extern "C" void* scalloc_calloc(size_t nmemb, size_t size) __THROW {
+  const size_t malloc_size = nmemb * size;
+  if (size != 0 && (malloc_size / size) != nmemb) {
+    return NULL;
+  }
+  void* result = scalloc::malloc(malloc_size);  // also sets errno
+  if (result != NULL) {
+    memset(result, 0, malloc_size);
+  }
+  return result;
+}
+
+extern "C" void* scalloc_realloc(void* ptr, size_t size) __THROW {
+  ErrorOut("realloc() not yet implemented.");
+}
+
+extern "C" void* scalloc_memalign(size_t __alignment, size_t __size) __THROW {
+  ErrorOut("memalign() not yet implemented.");
+}
+
+extern "C" int scalloc_posix_memalign(void** ptr, size_t align, size_t size) __THROW {
+  ErrorOut("posix_memalign() not yet implemented.");
+}
+
+extern "C" void* scalloc_valloc(size_t __size) __THROW {
+  ErrorOut("valloc() not yet implemented.");
+}
+
+extern "C" void* scalloc_pvalloc(size_t __size) __THROW {
+  ErrorOut("pvalloc() not yet implemented.");
+}
+
+extern "C" void scalloc_malloc_stats(void) __THROW {
+  ErrorOut("malloc_stats() not yet implemented.");
+}
+
+extern "C" int scalloc_mallopt(int cmd, int value) __THROW {
+  ErrorOut("mallopt() not yet implemented.");
 }
