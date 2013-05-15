cf0557e0ccea2df76a3f512d7053774c24f22103
Michael Lippautz <michael.lippautz@gmail.com>
2013-05-15 14:03:17 +0200

    More readable failed message.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/allocators/global_sbrk_allocator.cc
+++ src/allocators/global_sbrk_allocator.cc
@@ -4,6 +4,7 @@
 
 #include "allocators/global_sbrk_allocator.h"
 
+#include <errno.h>
 #include <sys/mman.h>  // mmap
 
 #include "log.h"
@@ -20,7 +21,7 @@ void GlobalSbrkAllocator::InitModule() {
 
   void* p = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (p == MAP_FAILED) {
-    ErrorOut("[GlobalSbrkAllocator] mmap failed");
+    ErrorOut("[GlobalSbrkAllocator] mmap failed. errno: %lu", errno);
   }
   current_ = reinterpret_cast<uintptr_t>(p);
 }
