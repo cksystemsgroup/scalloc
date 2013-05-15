b2867791115073990fd5001a1cccc277fba97198
Michael Lippautz <michael.lippautz@gmail.com>
2013-05-15 14:29:27 +0200

    Added a configure time flag to overwrite sbrk space.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/allocators/global_sbrk_allocator.cc
+++ src/allocators/global_sbrk_allocator.cc
@@ -7,19 +7,27 @@
 #include <errno.h>
 #include <sys/mman.h>  // mmap
 
+#include "common.h"
 #include "log.h"
+
+#ifdef SBRK_SPACE
+const size_t kMmapSize = SBRK_SPACE;
+#else  // SBRK_SPACE
+
+#ifdef __x86_64__
+const size_t kMmapSize = 1UL << 35;  // 32GiB
+#endif
+
+#ifdef __i386__
+const size_t kMmapSize = 1UL << 31;  // 2GiB
+#endif
+
+#endif
 
 uintptr_t GlobalSbrkAllocator::current_;
 
 void GlobalSbrkAllocator::InitModule() {
-#ifdef __x86_64__
-  size_t size = 1UL << 35;  // 32GiB
-#endif
-#ifdef __i386__
-  size_t size = 1UL << 31;
-#endif
-
-  void* p = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  void* p = mmap(0, kMmapSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (p == MAP_FAILED) {
     ErrorOut("[GlobalSbrkAllocator] mmap failed. errno: %lu", errno);
   }
