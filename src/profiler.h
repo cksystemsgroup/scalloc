4ed7888158e9440a8ccedec2e255a1238d73d9a5
Martin Aigner <martin@maigner.net>
2013-05-22 13:14:32 +0200

    support for medium object inter-arrival time

--- src/profiler.h
+++ src/profiler.h
@@ -12,6 +12,7 @@
 
 #include "common.h"
 #include "log.h"
+#include "random.h"
 
 #define SCALLOC_PROFILER_METHOD_GUARD \
     if (UNLIKELY(!enabled_)) return; \
@@ -45,6 +46,17 @@ class Profiler {
     allocated_bytes_count_ += size;
     if (Log2(size) > 31) ErrorOut("FUCK");
     sizeclass_histogram_[Log2(size)]++;
+
+    if (size > kMaxSmallSize && size <= kMaxMediumSize) {
+      if (UNLIKELY(medium_object_allocation_count_ == 0)) {
+        last_medium_object_timestamp_ = rdtsc();
+      }
+      uint64_t now = rdtsc();
+      sum_of_interarrival_times_ += now - last_medium_object_timestamp_;
+      last_medium_object_timestamp_ = now;
+      medium_object_allocation_count_++;
+    }
+
     if (UNLIKELY(allocated_bytes_count_ >= kTimeQuantum_)) {
       allocated_bytes_count_ = 0;
       PrintStatistics();
@@ -74,11 +86,12 @@ class Profiler {
 
  private:
   static const size_t kTimeQuantum_ = 1UL << 20;
+  static const size_t kLargeObject = (1UL << 9) + 1;
   static bool enabled_;
 
   FILE *fp_;
   pthread_t tid_;
-  bool self_allocating_;
+  bool self_allocating_; // to avoid loops while Init() calls a malloc
   uint32_t sizeclass_histogram_[33];  // 33 for everythong larger than 1<<32
   uint64_t block_stealing_count_;
   uint64_t sizeclass_refill_count_;
@@ -88,6 +101,10 @@ class Profiler {
   uint64_t warm_free_count_;
   uint64_t fast_free_count_;
   uint64_t slow_free_count_;
+
+  uint64_t last_medium_object_timestamp_;
+  uint64_t sum_of_interarrival_times_;
+  uint64_t medium_object_allocation_count_;
 
   void Init() {
     if (!enabled_) return;
@@ -110,14 +127,18 @@ class Profiler {
             "BS/A %3.1f%%; " 
             "H/FF %3.1f%%; " 
             "W/FF %3.1f%%; " 
-            "S/F %3.1f%%\n", 
+            "S/F %3.1f%%; "
+            "Medium/A %3.1f%%; "
+            "tMedium(kC) %3.1f\n", 
             tid_, allocation_count_, sizeclass_refill_count_, block_stealing_count_,
             free_count, hot_free_count_, warm_free_count_, slow_free_count_,
             100.0*(double)sizeclass_refill_count_/(double)allocation_count_,
             100.0*(double)block_stealing_count_/(double)allocation_count_,
             100.0*(double)hot_free_count_/(double)fast_free_count_,
             100.0*(double)warm_free_count_/(double)fast_free_count_,
-            100.0*(double)slow_free_count_/(double)free_count
+            100.0*(double)slow_free_count_/(double)free_count,
+            100.0*(double)medium_object_allocation_count_/(double)allocation_count_,
+            ((double)sum_of_interarrival_times_/(double)medium_object_allocation_count_)/1000
             );
     fprintf(fp_, "cont. SC-HISTO: ");
     for (unsigned i = 0; i < 33; ++i) {
