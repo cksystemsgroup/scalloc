bf61303a4b2290102135b633138f78362bc72b17
Michael Lippautz <michael.lippautz@gmail.com>
2013-05-27 17:48:50 +0200

    Added half-fit allocator.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

---
+++ src/allocators/half_fit.cc
@@ -0,0 +1,128 @@
+// Copyright (c) 2012-2013, the scalloc Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
+#include "allocators/half_fit.h"
+
+#include "common.h"
+#include "log.h"
+
+void HalfFit::Init(void* block, size_t len) {
+  for (size_t i = 0; i < kClasses; i++) {
+    flist_[i] = NULL;
+  }
+  used_ = 0;
+  uintptr_t ptr = reinterpret_cast<uintptr_t>(block);
+  const size_t span_hdr_sz = sizeof(this) +
+                             sizeof(ObjectHeader);  // including dummy header
+  ptr += span_hdr_sz;
+  len -= span_hdr_sz;
+  const size_t rest = kLargestSize - (ptr % kLargestSize);
+  ptr += rest;
+  len -= rest;
+  len -= kLargestSize;  // dummy trailer
+  ObjectHeader* oh = reinterpret_cast<ObjectHeader*>(
+      ptr - sizeof(ObjectHeader));
+  oh->used = true;  // set the dummy header
+  oh->sc = 42;
+  for (size_t i = 0; i < (len / kLargestSize); i++) {
+    oh = reinterpret_cast<ObjectHeader*>(ptr);
+    oh->Set(false, kClasses - 1);
+    GetFooter(oh)->Set(false, oh->sc);
+    ListAdd(GetListHeader(oh), oh->sc);
+    ptr += kLargestSize;
+  }
+  oh = reinterpret_cast<ObjectHeader*>(ptr);
+  oh->used = true;  // set the dummy trailer
+  oh->sc = 42;
+}
+
+void HalfFit::ListRemove(ListHeader* elem, const size_t sc) {
+  if (elem->prev) {
+    elem->prev->next = elem->next;
+  }
+  if (elem->next) {
+    elem->next->prev = elem->prev;
+  }
+  if (flist_[sc] == elem) {
+    flist_[sc] = elem->next;
+  }
+}
+
+void HalfFit::ListAdd(ListHeader* elem, const size_t sc) {
+  elem->next = flist_[sc];
+  elem->prev = NULL;
+  if (flist_[sc] != NULL) {
+    flist_[sc]->prev = elem;
+  }
+  flist_[sc] = elem;
+}
+
+HalfFit::ListHeader* HalfFit::ListGet(const size_t sc) {
+  if (flist_[sc] == NULL) {
+    return NULL;
+  }
+  ListHeader* elem = flist_[sc];
+  flist_[sc] = flist_[sc]->next;
+  return elem;
+}
+
+void* HalfFit::Allocate(const size_t size) {
+  size_t sc = SizeToSizeClass(size);
+  used_++;
+  ListHeader* elem = NULL;
+  for (size_t i = sc; i < kClasses; i++) {
+    elem = ListGet(i);
+    if (elem != NULL) {
+      ObjectHeader* oh = Split(GetObjectHeader(elem), i - sc);
+      elem = GetListHeader(oh);
+    }
+  }
+  return elem;
+}
+
+HalfFit::ObjectHeader* HalfFit::Split(ObjectHeader* oh, size_t level) {
+  if (level == 0) {
+    return oh;
+  }
+  oh->Set(false, oh->sc - 1);
+  GetFooter(oh)->Set(false, oh->sc);
+  ObjectHeader* right = reinterpret_cast<ObjectHeader*>(
+      reinterpret_cast<uintptr_t>(oh) + SizeClassToSize(oh->sc));
+  right->Set(false, oh->sc);
+  GetFooter(right)->Set(false, oh->sc);
+  ListAdd(GetListHeader(right), right->sc);
+  return Split(oh, level - 1);
+}
+
+void HalfFit::Free(void* p) {
+  used_--;
+  ListHeader* lh = reinterpret_cast<ListHeader*>(p);
+  TryMerge(GetObjectHeader(lh));
+}
+
+void HalfFit::TryMerge(ObjectHeader* oh) {
+  ObjectHeader* lh = GetLeftHeader(oh);
+  if (lh &&
+      oh->sc < (kClasses - 1) &&
+      lh->sc == oh->sc &&
+      (((1UL << (lh->sc + kMinShift)) - 1) &
+          reinterpret_cast<uintptr_t>(lh)) == 0) {
+    ListRemove(GetListHeader(lh), lh->sc);
+    oh = lh;
+    oh->Set(false, oh->sc + 1);
+    return TryMerge(oh);
+  }
+  ObjectHeader* rh = GetRightHeader(oh);
+  if (rh &&
+      oh->sc < (kClasses - 1) &&
+      rh->sc == oh->sc &&
+      (((1UL << (oh->sc + kMinShift)) - 1) &
+          reinterpret_cast<uintptr_t>(oh)) == 0) {
+    ListRemove(GetListHeader(rh), rh->sc);
+    oh->Set(false, oh->sc + 1);
+    GetFooter(oh)->Set(false, oh->sc);
+    return TryMerge(oh);
+  }
+  ListAdd(GetListHeader(oh), oh->sc);
+}
---
+++ src/allocators/half_fit.h
@@ -0,0 +1,104 @@
+// Copyright (c) 2012-2013, the scalloc Project Authors.  All rights reserved.
+// Please see the AUTHORS file for details.  Use of this source code is governed
+// by a BSD license that can be found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "common.h"
+
+class HalfFit {
+ public:
+  void Init(void* block, size_t len);
+  void* Allocate(const size_t size);
+  void Free(void* p);
+  bool Empty();
+
+ private:
+  static const size_t kMinShift = 10;
+  static const size_t kMinSize = 1UL << kMinShift;
+  static const size_t kClasses = 12;
+  static const size_t kLargestSize = 1UL << (kMinShift + kClasses - 1);
+
+  struct ObjectHeader {
+    bool used;
+    size_t sc;
+
+    void Set(bool used, size_t sc) {
+      this->used = used;
+      this->sc = sc;
+    }
+  };
+
+  struct ListHeader {
+    ListHeader* prev;
+    ListHeader* next;
+  };
+
+  ListHeader* flist_[kClasses];
+  size_t used_;
+
+  void ListRemove(ListHeader* elem, const size_t sc);
+  void ListAdd(ListHeader* elem, const size_t sc);
+  ListHeader* ListGet(const size_t sc);
+  size_t SizeToSizeClass(size_t size);
+  size_t SizeClassToSize(size_t sc);
+  ObjectHeader* GetLeftHeader(ObjectHeader* oh);
+  ObjectHeader* GetRightHeader(ObjectHeader* oh);
+  ObjectHeader* GetFooter(ObjectHeader* oh);
+  ObjectHeader* GetObjectHeader(ListHeader* lh);
+  ListHeader* GetListHeader(ObjectHeader* oh);
+  ObjectHeader* Split(ObjectHeader* oh, size_t level);
+  void TryMerge(ObjectHeader* oh);
+};
+
+inline size_t HalfFit::SizeToSizeClass(size_t size) {
+  size += 2 * sizeof(ObjectHeader);
+  return Log2(size - 1) + 1 - kMinShift;
+}
+
+inline size_t HalfFit::SizeClassToSize(size_t sc) {
+  return 1UL << (sc + kMinShift);
+}
+
+inline HalfFit::ObjectHeader* HalfFit::GetObjectHeader(ListHeader* lh) {
+  return reinterpret_cast<ObjectHeader*>(
+      reinterpret_cast<uintptr_t>(lh) - sizeof(ObjectHeader));
+}
+
+inline  HalfFit::ListHeader* HalfFit::GetListHeader(ObjectHeader* oh) {
+  return reinterpret_cast<ListHeader*>(
+      reinterpret_cast<uintptr_t>(oh) + sizeof(ObjectHeader));
+}
+
+inline HalfFit::ObjectHeader* HalfFit::GetLeftHeader(ObjectHeader* oh) {
+  oh = reinterpret_cast<ObjectHeader*>(
+      reinterpret_cast<uintptr_t>(oh) - sizeof(ObjectHeader));
+  if (oh->used == false) {
+    return reinterpret_cast<ObjectHeader*>(
+        reinterpret_cast<uintptr_t>(oh)
+        - SizeClassToSize(oh->sc)
+        + sizeof(ObjectHeader));
+  }
+  return NULL;
+}
+
+inline HalfFit::ObjectHeader* HalfFit::GetRightHeader(ObjectHeader* oh) {
+  oh = reinterpret_cast<ObjectHeader*>(
+      reinterpret_cast<uintptr_t>(oh) + SizeClassToSize(oh->sc));
+  if (oh->used == false) {
+    return oh;
+  }
+  return NULL;
+}
+
+inline HalfFit::ObjectHeader* HalfFit::GetFooter(ObjectHeader* oh) {
+  return reinterpret_cast<ObjectHeader*>(
+      reinterpret_cast<uintptr_t>(oh)
+      + SizeClassToSize(oh->sc)
+      - sizeof(ObjectHeader));
+}
+
+inline bool HalfFit::Empty() {
+  return used_ == 0;
+}
