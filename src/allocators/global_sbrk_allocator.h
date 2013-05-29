c5cd12d2e8ce2dc52393c81fab98cbabf25a6751
Michael Lippautz <michael.lippautz@gmail.com>
2013-05-29 14:05:39 +0200

    Add check for full arena.
    Signed-off-by: Michael Lippautz <michael.lippautz@gmail.com>

--- src/allocators/global_sbrk_allocator.h
+++ src/allocators/global_sbrk_allocator.h
@@ -31,7 +31,11 @@ always_inline bool GlobalSbrkAllocator::InArena(void* p) {
 }
 
 inline void* GlobalSbrkAllocator::Allocate(const size_t size) {
-  return reinterpret_cast<void*>(__sync_fetch_and_add(&current_, size));
+  void* p =  reinterpret_cast<void*>(__sync_fetch_and_add(&current_, size));
+  if (reinterpret_cast<uintptr_t>(p) > (start_ + size_)) {
+    ErrorOut("GlobalSbrkAllocator]: oom (arena full)");
+  }
+  return p;
 }
 
 inline void GlobalSbrkAllocator::Free(void* p, size_t len) {
