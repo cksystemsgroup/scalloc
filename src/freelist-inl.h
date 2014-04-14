1d3bd5560751919cd91ac0f73546b45c4115943b
Michael Lippautz <michael.lippautz@gmail.com>
2014-04-14 13:42:34 +0200

    Incremental freelist.

--- src/freelist-inl.h
+++ src/freelist-inl.h
@@ -20,7 +20,7 @@ class Freelist {
   void Push(void* p);
   void* Pop();
   size_t Utilization();
-  inline bool Empty() { return list_ == NULL; }
+  inline bool Empty() { return len_ == 0; }
   inline bool Full() { return len_ == cap_; }
   inline size_t Size() { return len_; }
 
@@ -28,6 +28,9 @@ class Freelist {
   size_t cap_;
   size_t len_;
   void* list_;
+
+  size_t size_class_;
+  void* bp_;
 
 #ifdef DEBUG
   uintptr_t lower_;
@@ -41,21 +44,25 @@ class Freelist {
 
 inline void Freelist::Init(const void* start,
                            const size_t size_class) {
+  size_class_ = size_class;
   len_ = ClassToObjects[size_class];
   cap_ = ClassToObjects[size_class];
   list_ = NULL;
-  uintptr_t start_ptr = reinterpret_cast<uintptr_t>(start);
-  const size_t size = ClassToSize[size_class];
 #ifdef DEBUG
+  const size_t size = ClassToSize[size_class];
+  uintptr_t start_ptr = reinterpret_cast<uintptr_t>(start);
   lower_ = start_ptr;
   upper_ = start_ptr + size * ClassToObjects[size_class];
 #endif  // DEBUG
+  bp_ = (void*)start;
+  /*
   for (size_t len = len_; len > 0; len--) {
     // Inlined Freelist::Push.
     *(reinterpret_cast<void**>(start_ptr)) = list_;
     list_ = reinterpret_cast<void*>(start_ptr);
     start_ptr += size;
   }
+  */
 }
 
 
@@ -74,6 +81,13 @@ inline void* Freelist::Pop() {
     ScallocAssert((reinterpret_cast<uintptr_t>(result) >= lower_) &&
                   (reinterpret_cast<uintptr_t>(result) < upper_));
     list_ = *(reinterpret_cast<void**>(list_));
+    len_--;
+  } else {
+    if (len_ == 0) {
+      return NULL;
+    }
+    result = bp_;
+    bp_ = (void*)(reinterpret_cast<uintptr_t>(bp_) + ClassToSize[size_class_]);
     len_--;
   }
   return result;
