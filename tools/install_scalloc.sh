376674272845b591bb285e068c4268c1647a80d7
Michael Lippautz <michael.lippautz@gmail.com>
2014-07-30 22:54:45 -0700

    New build configs.

--- tools/install_scalloc.sh
+++ tools/install_scalloc.sh
@@ -41,16 +41,20 @@ function build_config() {
   ln -s scalloc/out/Release/${name}.so ../${name}.so.0
 }
 
-
+build_config "libscalloc-hlab" "-D core_local=1 -D clab_policy=rr -D incremental_freelist=1"
+build_config "libscalloc-tlab" "-D core_local=-1 -D incremental_freelist=1"
 build_config "libscalloc-eager" "-D core_local=1 -D clab_policy=rr -D eager_madvise_threshold=4096 -D incremental_freelist=1"
-build_config "libscalloc-core-local" "-D core_local=1 -D clab_policy=utilization"
-build_config "libscalloc-core-local-1mb" "-D core_local=1 -D clab_policy=rr  -D size_classes_1mb=1 -D eager_madvise_threshold=4096"
-build_config "libscalloc-active-threads" "-D core_local=1 -D clab_policy=threads"
-build_config "libscalloc-static" "-D core_local=1 -D clab_policy=rr"
-build_config "libscalloc-hugepage" "-D core_local=1 -D clab_policy=rr -D huge_pages=1 -D incremental_freelist=1" 
-build_config "libscalloc-eager-reuse" "-D core_local=1 -D clab_policy=rr -D enable_slow_span_reuse=1"
-build_config "libscalloc-lazy-init" "-D core_local=1 -D clab_policy=rr -D enable_free_list_reuse=1"
-build_config "libscalloc-tlab" "-D core_local=-1 -D clab_policy=rr"
+build_config "libscalloc-hugepage" "-D core_local=1 -D clab_policy=rr -D size_class_config=hugepage -D incremental_freelist=1" 
+
+#build_config "libscalloc-eager" "-D core_local=1 -D clab_policy=rr -D eager_madvise_threshold=4096 -D incremental_freelist=1"
+#build_config "libscalloc-core-local" "-D core_local=1 -D clab_policy=utilization"
+#build_config "libscalloc-core-local-1mb" "-D core_local=1 -D clab_policy=rr  -D size_classes_1mb=1 -D eager_madvise_threshold=4096"
+#build_config "libscalloc-active-threads" "-D core_local=1 -D clab_policy=threads"
+#build_config "libscalloc-static" "-D core_local=1 -D clab_policy=rr"
+#build_config "libscalloc-hugepage" "-D core_local=1 -D clab_policy=rr -D huge_pages=1 -D incremental_freelist=1" 
+#build_config "libscalloc-eager-reuse" "-D core_local=1 -D clab_policy=rr -D enable_slow_span_reuse=1"
+#build_config "libscalloc-lazy-init" "-D core_local=1 -D clab_policy=rr -D enable_free_list_reuse=1"
+#build_config "libscalloc-tlab" "-D core_local=-1 -D clab_policy=rr"
 for i in 1 2 4 8 16 32 64
 do
   echo "test: $i"
