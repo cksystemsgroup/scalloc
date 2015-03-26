33ad68b5311fbbef0dca07a2cc6d724b1a8e12ba
Michael Lippautz <michael.lippautz@gmail.com>
2015-03-26 12:10:16 +0100

    Remove old version.

--- src/allocators/scalloc_core.cc
+++
@@ -1,8 +0,0 @@
-#include "scalloc_core-inl.h"
-
-namespace scalloc {
-
-TypedAllocator<ScallocCore>* ScallocCore::allocator;
-bool ScallocCore::enabled_;
-
-}  // namespace scalloc
--- tools/lint_scalloc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./tools/cpplint.py --filter=-build/header_guard,-build/include \
-  src/* \
-  src/allocators/* \
-  src/buffer/*
-
-exit $?
