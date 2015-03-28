3372b40bfbe9fc955e4a344f4b7cc9f122bbc7bc
Michael Lippautz <michael.lippautz@gmail.com>
2015-03-28 11:32:01 +0100

    update travis scripts.

--- tools/travis_before_install.sh
+++ tools/travis_before_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [[ $(uname -s) = "Darwin" ]]; then
+  echo "Darwin before install start"
+  echo "Darwin before install end"
 fi
 
 if [[ $(uname -s) = "Linux" ]]; then
+  echo "Linux before install start"
   # We need this to have g++4.8 available in apt
   sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt-get update -qq
+  echo "Linux before install end"
 fi
 
--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [[ $(uname -s) = "Darwin" ]]; then
+  echo "Darwin install start"
+  echo "Darwin install end"
 fi
 
 if [[ $(uname -s) = "Linux" ]]; then
+  echo "Linux install start"
   sudo apt-get install -qq gcc-4.8 g++-4.8 
   # We want to compile with g++ 4.8 when rather than the default g++
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+  echo "Linux install end"
 fi
 
